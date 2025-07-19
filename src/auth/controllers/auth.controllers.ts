728dc7265fca3377aa3cb85324e7476af26027e7
herlyn25 <herly-1988@hotmail.com>
2025-07-19 10:21:57 -0500

    Correccion del retorno del controller agregando la informacion de user

--- src/auth/controllers/auth.controllers.ts
+++ src/auth/controllers/auth.controllers.ts
@@ -1,5 +1,5 @@
 import { Controller, Body, Post, UnauthorizedException } from '@nestjs/common';
-import { AuthBody } from '../interfaces/auth.interfaces';
+
 import { AuthService } from '../services/auth.service';
 import { AuthDTO } from '../dto/auth.dto';
 import { ApiTags } from '@nestjs/swagger';
@@ -12,7 +12,11 @@ export class AuthControllers {
     @Post('login')
     async login(@Body() {username,password}: AuthDTO){
         const uservalidate = await this.authService.validateUser(username,password);
+        const token = await this.authService.generateJWT(uservalidate);
         if(!uservalidate) throw new UnauthorizedException("data no valid");
-        return await this.authService.generateJWT(uservalidate);
+        return {
+            "token": token.accessToken,
+            "user": uservalidate
+        };
     }
 }
