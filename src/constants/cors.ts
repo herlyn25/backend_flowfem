f7e280a2ecece3ace62c63d4395413470cfd5da8
herlyn25 <herly-1988@hotmail.com>
2025-07-24 18:59:43 -0500

    Corrigiendo los CORS y que implicitamente transforme los tipos de datos

--- src/constants/cors.ts
+++ src/constants/cors.ts
@@ -1,7 +1,11 @@
 import { CorsOptions } from "@nestjs/common/interfaces/external/cors-options.interface";
 
 export const CORS:CorsOptions = {
-    origin: true,
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials:true
+    credentials:true,
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+    exposedHeaders: 'Access-Control-Allow-Private-Network, Access-Control-Allow-Origin, Access-Control-Allow-Credentials',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
 }
